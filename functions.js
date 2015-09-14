11e31eba4e1eb3bd45aed71b429107bdb00c0904
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-09-14 18:13:05 +0000

    Created function 'compareStrings()' which allows .sort() to sort according to length of words.

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
-//this is a comment
+// #1
+// Array.prototype.sort method. 
+
+var randomWords = ["potato","apple","hamilton","ham","banana","burp","milestone","extraordinary","he"];
+
+console.log( randomWords.sort(compare) );
+
+function compare(a, b) {
+  if (a.length < b.length) {
+    return -1;
+  }
+  if (a.length > b.length) {
+    return 1;
+  }
+  // if a equal to b
+  return 0;
+}
